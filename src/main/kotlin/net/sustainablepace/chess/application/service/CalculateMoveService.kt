ea54660f3746d79c057acb8019e76b740f49ad91
christoph.baudson <christoph.baudson@rewe-digital.com>
2020-12-09 08:47:16 +0100

    simplified when

--- src/main/kotlin/net/sustainablepace/chess/application/service/CalculateMoveService.kt
+++ src/main/kotlin/net/sustainablepace/chess/application/service/CalculateMoveService.kt
@@ -12,16 +12,12 @@ import org.springframework.stereotype.Service
 class CalculateMoveService(val chessGameRepository: ChessGameRepository) : ApplicationService<CalculateMove, MoveCalculatedOrNot> {
     override fun process(intent: CalculateMove) = with(intent) {
         chessGameRepository.findById(chessGameId)?.let { chessGame ->
-            with(chessGame.activePlayer) {
-                when (this) {
-                    is ComputerPlayer -> calculateMove(chessGame).let { move ->
-                        when (move) {
-                            is ValidMove -> MoveCalculated(move, chessGame)
-                            else -> NoMoveCalculated("No move possible in game $chessGameId.")
-                        }
-                    }
-                    is HumanPlayer -> NoMoveCalculated("No move calculated, it's a human's turn!")
+            when(val player = chessGame.activePlayer) {
+                is ComputerPlayer -> when (val move = player.calculateMove(chessGame)) {
+                    is ValidMove -> MoveCalculated(move, chessGame)
+                    else -> NoMoveCalculated("No move possible in game $chessGameId.")
                 }
+                is HumanPlayer -> NoMoveCalculated("No move calculated, it's a human's turn!")
             }
         } ?: NoMoveCalculated("Could not find game $chessGameId.")
     }
