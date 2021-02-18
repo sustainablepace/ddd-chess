7fb7b36ed9b074399d5a2cde3f0996ef081f34c5
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2021-02-18 11:12:17 +0100

    basic domain model, part 2

--- src/main/kotlin/net/sustainablepace/chess/domain/ChessGame.kt
+++ src/main/kotlin/net/sustainablepace/chess/domain/ChessGame.kt
@@ -1,32 +1,82 @@
 package net.sustainablepace.chess.domain
 
+import net.sustainablepace.chess.domain.Color.*
+import net.sustainablepace.chess.domain.File.*
+import net.sustainablepace.chess.domain.Rank.*
+
 object Game
-object White
-object Black
 object Player
 object Computer
 object Human
 object Move
 
-sealed class Piece
-object King: Piece()
-object Queen: Piece()
-object Rook: Piece()
-object Bishop: Piece()
-object Knight: Piece()
-object Pawn: Piece()
+enum class Color {
+    WHITE, BLACK
+}
 
-typealias Position = Map<Square, Piece>
+sealed class Piece(val color: Color)
+class King(color: Color): Piece(color)
+class Queen(color: Color): Piece(color)
+class Rook(color: Color): Piece(color)
+class Bishop(color: Color): Piece(color)
+class Knight(color: Color): Piece(color)
+class Pawn(color: Color): Piece(color)
 
 enum class Rank {
     ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
 }
+
 enum class File {
     A, B, C, D, E, F, G, H
 }
+
 typealias Square = Pair<File, Rank>
 
-object InitialPosition
+typealias Position = Map<Square, Piece>
+
+val initialPosition: Position =
+    mapOf(
+        Square(A, ONE) to Rook(WHITE),
+        Square(B, ONE) to Knight(WHITE),
+        Square(C, ONE) to Bishop(WHITE),
+        Square(D, ONE) to Queen(WHITE),
+        Square(E, ONE) to King(WHITE),
+        Square(F, ONE) to Bishop(WHITE),
+        Square(G, ONE) to Knight(WHITE),
+        Square(H, ONE) to Rook(WHITE),
+        /*
+        "c1" to "wB",
+        "d1" to "wQ",
+        "e1" to "wK",
+        "f1" to "wB",
+        "g1" to "wN",
+        "h1" to "wR",
+        "a2" to "wP",
+        "b2" to "wP",
+        "c2" to "wP",
+        "d2" to "wP",
+        "e2" to "wP",
+        "f2" to "wP",
+        "g2" to "wP",
+        "h2" to "wP",
+        "a8" to "bR",
+        "b8" to "bN",
+        "c8" to "bB",
+        "d8" to "bQ",
+        "e8" to "bK",
+        "f8" to "bB",
+        "g8" to "bN",
+        "h8" to "bR",
+        "a7" to "bP",
+        "b7" to "bP",
+        "c7" to "bP",
+        "d7" to "bP",
+        "e7" to "bP",
+        "f7" to "bP",
+        "g7" to "bP",
+        "h7" to "bP"
+         */
+    )
 
 
 
