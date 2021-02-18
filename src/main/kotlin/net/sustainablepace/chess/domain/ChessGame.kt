7266d24c38f66fbedd2d1ea2f59559b749a3478f
Arne Schubert <arne.schubert@rewe-digital.com>
2021-02-18 11:36:27 +0100

    First steps on implementing the game class

--- src/main/kotlin/net/sustainablepace/chess/domain/ChessGame.kt
+++ src/main/kotlin/net/sustainablepace/chess/domain/ChessGame.kt
@@ -4,11 +4,9 @@ import net.sustainablepace.chess.domain.Color.*
 import net.sustainablepace.chess.domain.File.*
 import net.sustainablepace.chess.domain.Rank.*
 
-object Game
 object Player
 object Computer
 object Human
-object Move
 
 enum class Color {
     WHITE, BLACK
@@ -30,9 +28,15 @@ enum class File {
     A, B, C, D, E, F, G, H
 }
 
+enum class GameState {
+    IN_PROGRESS, DRAW, WHITE_WINS, BLACK_WINS
+}
+
 typealias Square = Pair<File, Rank>
 
 typealias Position = Map<Square, Piece>
+
+typealias Move = Pair<Position, Position>
 
 val initialPosition: Position =
     mapOf(
@@ -44,42 +48,43 @@ val initialPosition: Position =
         Square(F, ONE) to Bishop(WHITE),
         Square(G, ONE) to Knight(WHITE),
         Square(H, ONE) to Rook(WHITE),
-        /*
-        "c1" to "wB",
-        "d1" to "wQ",
-        "e1" to "wK",
-        "f1" to "wB",
-        "g1" to "wN",
-        "h1" to "wR",
-        "a2" to "wP",
-        "b2" to "wP",
-        "c2" to "wP",
-        "d2" to "wP",
-        "e2" to "wP",
-        "f2" to "wP",
-        "g2" to "wP",
-        "h2" to "wP",
-        "a8" to "bR",
-        "b8" to "bN",
-        "c8" to "bB",
-        "d8" to "bQ",
-        "e8" to "bK",
-        "f8" to "bB",
-        "g8" to "bN",
-        "h8" to "bR",
-        "a7" to "bP",
-        "b7" to "bP",
-        "c7" to "bP",
-        "d7" to "bP",
-        "e7" to "bP",
-        "f7" to "bP",
-        "g7" to "bP",
-        "h7" to "bP"
-         */
+
+        Square(A, TWO) to Pawn(WHITE),
+        Square(B, TWO) to Pawn(WHITE),
+        Square(C, TWO) to Pawn(WHITE),
+        Square(D, TWO) to Pawn(WHITE),
+        Square(E, TWO) to Pawn(WHITE),
+        Square(F, TWO) to Pawn(WHITE),
+        Square(G, TWO) to Pawn(WHITE),
+        Square(H, TWO) to Pawn(WHITE),
+
+        Square(A, SEVEN) to Pawn(BLACK),
+        Square(B, SEVEN) to Pawn(BLACK),
+        Square(C, SEVEN) to Pawn(BLACK),
+        Square(D, SEVEN) to Pawn(BLACK),
+        Square(E, SEVEN) to Pawn(BLACK),
+        Square(F, SEVEN) to Pawn(BLACK),
+        Square(G, SEVEN) to Pawn(BLACK),
+        Square(H, SEVEN) to Pawn(BLACK),
+
+        Square(A, EIGHT) to Rook(BLACK),
+        Square(B, EIGHT) to Knight(BLACK),
+        Square(C, EIGHT) to Bishop(BLACK),
+        Square(D, EIGHT) to Queen(BLACK),
+        Square(E, EIGHT) to King(BLACK),
+        Square(F, EIGHT) to Bishop(BLACK),
+        Square(G, EIGHT) to Knight(BLACK),
+        Square(H, EIGHT) to Rook(BLACK),
     )
 
+class Game(
+        val position: Position,
+        val id: ChessGameId, // todo: maybe change to uuid later
+        val state: GameState = GameState.IN_PROGRESS,
+        val activeColor: Color,
+) {
 
-
+}
 
 class ChessGame private constructor(
     val id: ChessGameId,
