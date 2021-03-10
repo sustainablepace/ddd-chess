6da77e6a3958e9e70c8f04e968ace9b8e6a26c67
Jürgen Loho <juergen.loho@rewe-digital.com>
2021-03-10 14:49:11 +0100

    Update Domain.kt

--- src/main/kotlin/net/sustainablepace/chess/Domain.kt
+++ src/main/kotlin/net/sustainablepace/chess/Domain.kt
@@ -2,22 +2,48 @@ package net.sustainablepace.chess
 
 typealias Schach = Boolean
 object Stellung
-object AktiveSeite
+object AktiveSeite: Seite
+
+enum class Seite {
+    weiss, schwarz
+}
+
+enum class Aktion {
+    Zug, RemisAnbieten, aufgeben
+}
+
 class Zustand {
     val schach = Schach
     val stellung = Stellung
     val aktiveSeite = AktiveSeite
 }
+
 object AktuellerZustand: Zustand
 typealias Zustandshistorie = List<Zustand>
-class Spieler {
+typealias Aktionshistorie = List<Aktion>
 
+enum class SpielerStatus {
+    aktiv, inaktiv
 }
-object Ergebnis
+class Spieler(val seite: Seite) {
+    var status: SpielerStatus = SpielerStatus.inaktiv
+}
+sealed class Ergebnis
+
+object Schachmatt: Ergebnis()
+
+class Remis: Ergebnis() {
+    val grund: RemisGrund
+}
+
+enum class RemisGrund {
+    Patt, Einigung, Stellungswiederholung
+}
 
 class Partie {
     val aktuellerZustand = AktuellerZustand
     val zustandshistorie = Zustandshistorie
+    val aktionshistorie = Aktionshistorie
     val spielerInWeiss = Spieler()
     val spielerInSchwarz = Spieler()
     val ergebnis = Ergebnis
